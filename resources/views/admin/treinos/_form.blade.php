637d7699362391d425e17828b049b222f547773f
MarcioPaula <marciogspaula@gmail.com>
2022-06-27 10:32:50 -0300

    Update _form.blade.php

--- resources/views/admin/treinos/_form.blade.php
+++ resources/views/admin/treinos/_form.blade.php
@@ -1,19 +1,19 @@
 <div class="input-field">
-    <input type="text" autocomplete="off" name="treino" value="{{isset($registro -> treino) ? $registro -> treino : ''}}">
+    <input type="text" autocomplete="off" required name="treino" value="{{isset($registro -> treino) ? $registro -> treino : ''}}">
     <label>Treino</label>
 </div>
 
 <div class="input-field">
-    <input type="text" autocomplete="off" name="descricao" value="{{isset($registro -> descricao) ? $registro -> descricao : ''}}">
+    <input type="text" autocomplete="off" required name="descricao" value="{{isset($registro -> descricao) ? $registro -> descricao : ''}}">
     <label>Descrição</label>
 </div>
 
     <div class="file-field input-field">
         <div class="btn">
-            <span>File</span>
-            <input name="arquivo" type="file" value="{{isset($registro -> arquivo) ? $registro -> arquivo : ''}}">
+            <span>Arquivo</span>
+            <input name="arquivo" required type="file" value="{{isset($registro -> arquivo) ? $registro -> arquivo : ''}}">
         </div>
         <div class="file-path-wrapper">
-            <input class="file-path validate" type="text">
+            <input class="file-path validate" type="text" value="{{isset($registro -> arquivo) ? $registro -> arquivo : ''}}">
         </div>
     </div>

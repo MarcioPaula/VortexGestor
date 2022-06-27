103a29b8b121128fc3b6dd25cf5e7deae49c5143
MarcioPaula <marciogspaula@gmail.com>
2022-06-27 10:32:46 -0300

    Update _form.blade.php

--- resources/views/admin/alunos/_form.blade.php
+++ resources/views/admin/alunos/_form.blade.php
@@ -1,16 +1,16 @@
 <div class="input-field">
-    <input type="text" autocomplete="off" name="nome" value="{{isset($registro -> nome) ? $registro -> nome : ''}}">
+    <input type="text" required autocomplete="off" name="nome" value="{{isset($registro -> nome) ? $registro -> nome : ''}}">
     <label>Nome</label>
 </div>
 
 <div class="input-field">
-    <input type="text" autocomplete="off" name="cpf" value="{{isset($registro -> cpf) ? $registro -> cpf : ''}}">
+    <input type="text" required autocomplete="off" name="cpf" value="{{isset($registro -> cpf) ? $registro -> cpf : ''}}">
     <label>CPF</label>
 </div>
 
 <div class="input-field col s12">
-    <select name="treino">
-        <option value="{{isset($registro -> treino) ? $registro -> treino : ''}}"  disabled selected>Choose your option</option>
+    <select name="treino" required>
+        <option value="{{isset($registro -> treino) ? $registro -> treino : ''}}"  disabled selected>{{isset($registro -> treino) ? $registro -> treino : 'Selecione um treino'}}</option>
          @foreach($treinos as $treino);
         <option value="{{$treino->treino}}">{{$treino->treino}}</option>
         @endforeach
